4f848346f9e8d21f297e51922ceec89a5f4d0f6d
cty7574 <vicpool@naver.com>
2025-11-05 19:02:21 +0900

    테이블뷰 제약 정의

--- 1.ExpandableCardView/ExpandableCardView/ViewController.swift
+++ 1.ExpandableCardView/ExpandableCardView/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tv: UITableView = UITableView()
+        tv.backgroundColor = .clear
+        tv.isScrollEnabled = false
+        tv.separatorStyle = .none
+        tv.backgroundColor = .red
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
-
 }
 
+#Preview {
+    ViewController()
+}
