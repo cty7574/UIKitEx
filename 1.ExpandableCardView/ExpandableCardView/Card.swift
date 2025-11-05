babc7441be5da58d6b028d8da27a1857970d77d7
cty7574 <vicpool@naver.com>
2025-11-05 19:07:55 +0900

    카드 모델 정의

---
+++ 1.ExpandableCardView/ExpandableCardView/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  ExpandableCardView
+//
+//  Created by 멘태 on 11/5/25.
+//
+
+struct Card {
+    let title: String
+    let content: String
+    let profileImage: String
+    var isExpand: Bool = false
+}
