bc4a19d106905a6dd73f585c15558da48a3a203b
cty7574 <vicpool@naver.com>
2025-11-17 21:02:47 +0900

    headerview UI 구성

---
+++ 2.StickyHeader/StickyHeader/Views/HeaderView.swift
@@ -0,0 +1,124 @@
+//
+//  HeaderView.swift
+//  StickyHeader
+//
+//  Created by 멘태 on 11/17/25.
+//
+
+import UIKit
+
+final class HeaderView: UIView {
+    private let mainImage: UIImageView = {
+        let iv: UIImageView = UIImageView()
+        iv.image = UIImage(systemName: "globe")
+        iv.contentMode = .scaleAspectFill
+        iv.tintColor = .black
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Apple Foods"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let starImageView: UIImageView = {
+        let iv: UIImageView = UIImageView()
+        iv.image = UIImage(systemName: "star.fill")
+        iv.contentMode = .scaleAspectFill
+        iv.tintColor = .black
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private let ratingLabel: UILabel = {
+        let label: UILabel = UILabel()
+        var attributedString: NSMutableAttributedString = .init(string: "4.5 ")
+        attributedString.append(
+            NSAttributedString(
+                string: "(20K ratings) ",
+                attributes: [
+                    .font: UIFont.boldSystemFont(ofSize: 16)
+                ]
+            )
+        )
+        label.attributedText = attributedString
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let timeImageView: UIImageView = {
+        let iv: UIImageView = UIImageView()
+        iv.image = UIImage(systemName: "clock")
+        iv.contentMode = .scaleAspectFill
+        iv.tintColor = .black
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label: UILabel = UILabel()
+        var attributedString: NSMutableAttributedString = .init(string: "35-40 ")
+        attributedString.append(
+            NSAttributedString(
+                string: "Mins",
+                attributes: [
+                    .font: UIFont.boldSystemFont(ofSize: 16)
+                ]
+            )
+        )
+        label.attributedText = attributedString
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var infomationStackView: UIStackView = {
+        let st: UIStackView = UIStackView(arrangedSubviews: [starImageView, ratingLabel, timeImageView, timeLabel])
+        st.axis = .horizontal
+        st.spacing = 12
+        st.distribution = .fillProportionally
+        st.alignment = .center
+        st.translatesAutoresizingMaskIntoConstraints = false
+        return st
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.addSubview(mainImage)
+        self.addSubview(titleLabel)
+        self.addSubview(infomationStackView)
+        
+        NSLayoutConstraint.activate([
+            mainImage.topAnchor.constraint(equalTo: self.topAnchor),
+            mainImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            mainImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            mainImage.heightAnchor.constraint(equalToConstant: 250),
+            
+            titleLabel.topAnchor.constraint(equalTo: mainImage.bottomAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: mainImage.leadingAnchor),
+            
+            infomationStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            infomationStackView.leadingAnchor.constraint(equalTo: mainImage.leadingAnchor),
+            infomationStackView.trailingAnchor.constraint(equalTo: mainImage.trailingAnchor),
+            infomationStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            
+            starImageView.widthAnchor.constraint(equalToConstant: 15),
+            starImageView.heightAnchor.constraint(equalTo: starImageView.widthAnchor),
+            
+            timeImageView.widthAnchor.constraint(equalTo: starImageView.widthAnchor),
+            timeImageView.heightAnchor.constraint(equalTo: starImageView.heightAnchor)
+        ])
+    }
+}
--- 2.StickyHeader/StickyHeader/Views/ViewController.swift
+++ 2.StickyHeader/StickyHeader/Views/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 final class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
     }
 }
 
+extension ViewController {
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header: HeaderView = .init()
+        return header
+    }
+}
