2c0ad839c717ead798876df20907cc11f7780229
cty7574 <vicpool@naver.com>
2025-11-17 20:28:23 +0900

    코드베이스 세팅

--- 2.StickyHeader/StickyHeader/Views/ViewController.swift
+++ 2.StickyHeader/StickyHeader/Views/ViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
